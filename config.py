c66db205035be7953cc00e9d03ff6562415c6d71
mateoguero <mateogabrielaguero15@gmail.com>
2026-09-19 21:51:07 -0300

    Update config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Configuración de la Base de Datos
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'SistemaAutocor',
+    'port': 3306
+}
+
+# Constantes de la aplicación
+APP_TITLE = "AUTOCOR | Taller & Repuestera"
+APP_GEOMETRY = "1024x600"
